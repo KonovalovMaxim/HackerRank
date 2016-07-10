beac55a4b81521472e7c5327603281662d1f108f
Konovalov Maxim <komaniks@gmail.com>
2016-07-11 05:01:05 +0900

    Bit Manipulation / Maximizing XOR

---
+++ Bit Manipulation/Maximizing XOR/src/Solution.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+
+public class Solution {
+
+    static int maxXor(int l, int r) {
+        int max = 0;
+        for (int i = l; i <= r; i++) {
+            for (int j = l; j <= r; j++) {
+                max = Math.max(i ^ j, max);
+            }
+        }
+        return max;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int res;
+        int _l;
+        _l = Integer.parseInt(in.nextLine());
+
+        int _r;
+        _r = Integer.parseInt(in.nextLine());
+
+        res = maxXor(_l, _r);
+        System.out.println(res);
+
+    }
+}
---
+++ Bit Manipulation/Maximizing XOR/test/SolutionTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+    @Test
+    public void maxXor() throws Exception {
+        assertEquals(15, Solution.maxXor(1, 10));
+    }
+
+}
